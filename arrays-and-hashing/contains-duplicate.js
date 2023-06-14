fdd33243336a1d199b4b359852c0b5636460497e
Himanshu Koshti <starmechanism@proton.me>
2023-06-14 23:38:51 +0530

    01 - Contains Duplicate

---
+++ arrays-and-hashing/contains-duplicate.js
@@ -0,0 +1,24 @@
+// Given an integer array nums, return true if any value appears at
+// least twice in the array, and return false if every element is distinct.
+// Example 1:
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+// Constraints:
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+function containsDuplicate(nums) {
+    if (nums.length === (new Set(nums)).size) {
+        return false;
+    }
+    return true;
+}
+console.log(containsDuplicate([1, 2, 3, 4, 5]));
+console.log(containsDuplicate([1, 2, 3, 4, 5, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4, 5, 5]));
+console.log(containsDuplicate([1, 2, 3, 4, 5, 34, 7, 8, 9, 10, 11, 12, 2]));
